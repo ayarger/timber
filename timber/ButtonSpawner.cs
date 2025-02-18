aa63216c05ff9ca982297442c1814c2de5aa932c
shalimma <shalimma@umich.edu>
2025-02-17 19:33:13 -0500

    Loads Chunk as button icon

--- timber/ButtonSpawner.cs
+++ timber/ButtonSpawner.cs
@@ -1,4 +1,7 @@
 using Godot;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
 
 public class ButtonSpawner : Control
 {
@@ -6,11 +9,16 @@ public class ButtonSpawner : Control
     private Timer _timer;
     private int _buttonCount = 1;
 
+    private Texture _buttonIconTexture;
+
+    //private const string ImageFilePath = "resources/images/chunk_idle.png";
+    private const string ImageFilePath = "images/chunk_idle.png";
+
+
     public override void _Ready()
     {
         GD.Print("ButtonSpawner _Ready() is running!");
 
-        // Locate the GridContainer inside SpritesTab
         _gridContainer = GetNode<GridContainer>("ScrollContainer/GridContainer");
 
         if (_gridContainer == null)
@@ -21,7 +29,6 @@ public class ButtonSpawner : Control
 
         GD.Print("GridContainer found, setting up Timer...");
 
-        // Create and configure the Timer
         _timer = new Timer();
         _timer.WaitTime = 2.0f; // 2 seconds
         _timer.Autostart = true;
@@ -29,14 +36,66 @@ public class ButtonSpawner : Control
         _timer.Connect("timeout", this, nameof(OnTimerTimeout));
         AddChild(_timer);
 
-        GD.Print("Timer started, buttons will spawn every 2 seconds...");
+        GD.Print("Timer Started!");
+
+        ArborCoroutine.StartCoroutine(LoadButtonIcon(), this);
+    }
+
+    IEnumerator LoadButtonIcon()
+    {
+        GD.Print($"Loading icon from path: {ImageFilePath}");
+
+        // ArborResource.Load<ModFileManifest>("mod_file_manifest.json");
+        // yield return ArborResource.WaitFor("mod_file_manifest.json");
+        // ModFileManifest manifest = ArborResource.Get<ModFileManifest>("mod_file_manifest.json");
+
+        //   // try to find all png files in images folder
+
+        // List<string> spriteFiles = manifest.Search("resources/images/.*\\.png");
+
+        //ArborResource.Load<Texture>(ImageFilePath);
+        //yield return ArborResource.WaitFor(ImageFilePath);
+
+        ArborResource.UseResource(
+			ImageFilePath,
+			(Texture texture) => {
+				Icon title_logo_node = GetNode<Icon>("Button");
+				_buttonIconTexture = texture;
+			},
+			this
+		);
+
+        _buttonIconTexture = ArborResource.Get<Texture>(ImageFilePath);
+
+        if (_buttonIconTexture == null)
+        {
+            GD.PrintErr($"Failed to load image from path: {ImageFilePath}");
+            yield break;
+        }
+
+        GD.Print("Button icon successfully loaded!");
+
+        //_timer.Start();
     }
 
     private void OnTimerTimeout()
     {
+        
+        SpawnButton();
+    }
+
+    private void SpawnButton()
+    {
         Button newButton = new Button();
         newButton.Text = $"Button {_buttonCount}";
         newButton.RectMinSize = new Vector2(100, 100);
+
+        // try set icon
+        if (_buttonIconTexture != null)
+        {
+            newButton.Icon = _buttonIconTexture;
+        }
+
         newButton.Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array { _buttonCount });
 
         _gridContainer.AddChild(newButton);
--- timber/SpriteLoader.cs
+++ timber/SpriteLoader.cs
@@ -11,46 +11,39 @@ public class SpriteLoader : Control
     {
         AddTextButton("SpriteLoader Ready!");
         GD.Print("SpriteLoader Ready!");
-        // Locate the GridContainer inside the Sprites tab
         _gridContainer = GetNode<GridContainer>("ScrollContainer/GridContainer");
 
-        // Start fetching images from AWS S3 using ArborResource
         ArborCoroutine.StartCoroutine(LoadSprites(), this);
     }
 
+    // attempts to load all png files from ArborResource
     IEnumerator LoadSprites()
     {
-        // Step 1: Load the mod file manifest to retrieve available image files
         ArborResource.Load<ModFileManifest>("mod_file_manifest.json");
         yield return ArborResource.WaitFor("mod_file_manifest.json");
         ModFileManifest manifest = ArborResource.Get<ModFileManifest>("mod_file_manifest.json");
 
-        // Step 2: Filter for PNG files in the "resources/images/" folder
+        // try to find all png files in images folder
+
         List<string> spriteFiles = manifest.Search("resources/images/.*\\.png");
 
         GD.Print($"Found {spriteFiles.Count} sprite images in S3 bucket");
         AddTextButton($"Found {spriteFiles.Count} sprite images in S3 bucket");
 
-        // Step 3: Load each PNG file asynchronously
         foreach (string spriteFile in spriteFiles)
         {
             ArborResource.Load<Texture>(spriteFile);
         }
 
-        // Step 4: Wait for each image to be loaded and create buttons
         foreach (string spriteFile in spriteFiles)
         {
             yield return ArborResource.WaitFor(spriteFile);
 
             Texture texture = ArborResource.Get<Texture>(spriteFile);
             if (texture != null)
-            {
                 AddSpriteButton(spriteFile, texture);
-            }
             else
-            {
                 GD.PrintErr($"Failed to load texture: {spriteFile}");
-            }
         }
     }
 
@@ -66,23 +59,20 @@ public class SpriteLoader : Control
 
     void AddSpriteButton(string spritePath, Texture texture)
     {
-        // Create a new button
         Button spriteButton = new Button();
-        spriteButton.RectMinSize = new Vector2(100, 100); // Set button size
-        spriteButton.Icon = texture; // Set the image as button icon
-        spriteButton.HintTooltip = spritePath; // Tooltip for image name
+        spriteButton.RectMinSize = new Vector2(100, 100);
+        spriteButton.Icon = texture;
+        spriteButton.HintTooltip = spritePath;
 
-        // Connect button click event
         spriteButton.Connect("pressed", this, nameof(OnSpriteButtonPressed), new Godot.Collections.Array { spritePath });
 
-        // Add to the grid container
         _gridContainer.AddChild(spriteButton);
     }
 
     void OnSpriteButtonPressed(string spritePath)
     {
         GD.Print($"Sprite button clicked: {spritePath}");
-        // Handle sprite selection logic here
+        // fake debug
         AddTextButton("Button Clicked!");
     }
 }
